bd73897d2856d25dfb4972d91cfdcc0199abad72
xzxzzz <1254345012@qq.com>
2023-12-08 21:12:25 +0800

    更新ClassService

--- Back/sign-in-backend/src/main/java/com/runde/signinbackend/service/ClassService.java
+++ Back/sign-in-backend/src/main/java/com/runde/signinbackend/service/ClassService.java
@@ -1,7 +1,14 @@
 package com.runde.signinbackend.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.runde.signinbackend.model.dto.classInfo.ClassQueryListRequest;
 import com.runde.signinbackend.model.entity.Class;
+import com.runde.signinbackend.model.vo.ClassVO;
 
 public interface ClassService extends IService<Class> {
+
+    ClassVO getClassVO(Class classInfo);
+
+    QueryWrapper<Class> getQueryWrapper(ClassQueryListRequest classQueryListRequest);
 }
--- Back/sign-in-backend/src/main/java/com/runde/signinbackend/service/impl/ClassServiceImpl.java
+++ Back/sign-in-backend/src/main/java/com/runde/signinbackend/service/impl/ClassServiceImpl.java
@@ -1,12 +1,47 @@
 package com.runde.signinbackend.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.runde.signinbackend.common.ErrorCode;
+import com.runde.signinbackend.constant.CommonConstant;
+import com.runde.signinbackend.exception.BusinessException;
 import com.runde.signinbackend.mapper.ClassMapper;
+import com.runde.signinbackend.model.dto.classInfo.ClassQueryListRequest;
 import com.runde.signinbackend.model.entity.Class;
+import com.runde.signinbackend.model.vo.ClassVO;
 import com.runde.signinbackend.service.ClassService;
+import com.runde.signinbackend.utils.SqlUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClassServiceImpl extends ServiceImpl<ClassMapper, Class> implements ClassService {
 
+    @Override
+    public ClassVO getClassVO(Class classInfo) {
+        return classInfo == null? null : BeanUtil.copyProperties(classInfo, ClassVO.class);
+    }
+
+    @Override
+    public QueryWrapper<Class> getQueryWrapper(ClassQueryListRequest classQueryListRequest) {
+
+        if (classQueryListRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        String name = classQueryListRequest.getName();
+        String profile = classQueryListRequest.getProfile();
+        String code = classQueryListRequest.getCode();
+        Integer createUser = classQueryListRequest.getCreateUser();
+        Integer isDelete = classQueryListRequest.getIsDelete();
+        String sortField = classQueryListRequest.getSortField();
+        String sortOrder = classQueryListRequest.getSortOrder();
+        QueryWrapper<Class> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like(name != null,"name",name);
+        queryWrapper.like(profile != null,"profile",profile);
+        queryWrapper.like(code != null,"code",code);
+        queryWrapper.eq(createUser != null,"create_user",createUser);
+        queryWrapper.eq(isDelete != null, "is_delete", isDelete);
+        queryWrapper.orderBy(SqlUtil.validSortField(sortField), sortOrder.equals(CommonConstant.SORT_ORDER_ASC), sortField);
+        return queryWrapper;
+    }
 }
